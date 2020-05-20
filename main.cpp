b9daabc63eafeb62bd0015d1b15c88ec9ed5c032
deanik <62985319+deanik@users.noreply.github.com>
2020-05-20 09:43:15 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+        ifstream wej;
+        ofstream wyj;
+        wej.open("wej.txt");
+        wyj.open("wyj.txt");
+        int c = 0;
+        int a[1000];
+        if (wej.good()) 
+		{
+            while (!wej.eof()) 
+			{
+                wej >> a[c++];
+            }
+        }
+        else { cout << "Tekst"; }
+        if (wyj.good())
+		{
+            for (int i = c - 2; i >= 0; i--)
+			{
+                cout << a[i];
+                wyj << a[i] << " ";
+            }
+        }
+        else { cout << "Tekst2"; }
+        wej.close();
+        wyj.close();
+}
+        return 0;
